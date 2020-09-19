c0d11856f0f072522bc06ebeb50ec72944cfa80f
ShaluRose <36034406+ShaluRose@users.noreply.github.com>
2020-09-19 11:36:47 +0530

    Updates

---
+++ precache-manifest.9b9c6dd937a5d59bc30552d8cd617e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cc16f0ece6e1a549bfdbd95147b6e9",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "6230e3943f2f7bc31dbd",
+    "url": "/quiz-app/static/css/main.8781fd71.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/quiz-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6230e3943f2f7bc31dbd",
+    "url": "/quiz-app/static/js/main.0a6b21ff.chunk.js"
+  },
+  {
+    "revision": "b2c0cc7b0c080cbe8f03",
+    "url": "/quiz-app/static/js/runtime-main.69e9b983.js"
+  }
+]);
